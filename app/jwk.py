332956e55338d9425d5bee848034313d7e220cce
SA2436 <stephenalonso@my.unt.edu>
2025-09-21 00:29:02 -0500

    Update jwk.py

--- app/jwk.py
+++ app/jwk.py
@@ -1,21 +1,16 @@
 # app/jwk.py
-from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicNumbers
-from typing import Dict
-import base64
+import jwt
 
-def _b64url(b: bytes) -> str:
-    return base64.urlsafe_b64encode(b).rstrip(b"=").decode("ascii")
+def rsa_key_to_jwk(key):
+    public_numbers = key.public_key.public_numbers()
+    e = public_numbers.e
+    n = public_numbers.n
 
-def rsa_public_to_jwk(kid: str, pub_numbers: RSAPublicNumbers) -> Dict:
-    n = pub_numbers.n
-    e = pub_numbers.e
-    n_bytes = n.to_bytes((n.bit_length() + 7) // 8, "big")
-    e_bytes = e.to_bytes((e.bit_length() + 7) // 8, "big")
     return {
         "kty": "RSA",
-        "use": "sig",
         "alg": "RS256",
-        "kid": kid,
-        "n": _b64url(n_bytes),
-        "e": _b64url(e_bytes),
+        "use": "sig",
+        "kid": key.kid,
+        "n": jwt.utils.base64url_encode(n.to_bytes((n.bit_length() + 7) // 8, "big")).decode("utf-8"),
+        "e": jwt.utils.base64url_encode(e.to_bytes((e.bit_length() + 7) // 8, "big")).decode("utf-8")
     }
